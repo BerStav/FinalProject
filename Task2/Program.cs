339b158f9973b03e6b35228c462e69bf2cf9245e
Vitaly <Vitalion.Bersenev@gmail.com>
2022-12-20 00:50:48 +0700

    task2 done

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдет сумму натуральных элементов в промежутке от M до N.
+
+Console.Write("Please, enter the first number :");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.Write("Please, enter the second number :");
+int numberN = int.Parse(Console.ReadLine());
+
+int max = numberN;
+
+if (numberM > numberN)
+{
+    numberN = numberM;
+    numberM = max;
+}
+
+int SumNumber(int start, int end)
+{
+    if (start == end) return start;
+    return (start + SumNumber(start + 1, end));
+}
+
+Console.WriteLine(SumNumber(numberM, numberN));
